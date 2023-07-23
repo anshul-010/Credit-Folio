75ff8c08631f806c6db950bfb74ca5e4a90f4ea2
shivamvr <shivamweb2@gmail.com>
2023-07-23 21:33:42 +0530

    minor changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,72 +1,96 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { styled } from 'styled-components'
+
+
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Card = (props) => {
-  const isAdmin = useSelector(state =>state.authReducer.isAdmin)
-  console.log('isAdmin',isAdmin)
+  const isAdmin = useSelector((state) => state.authReducer.isAdmin);
+  console.log("isAdmin", isAdmin);
   const navigate = useNavigate();
-  const editHandler = ()=>{
-    navigate('/admin/edit/:id')
-  }
+  const editHandler = () => {
+    navigate("/admin/edit/:id");
+  };
+  const pStyle = {
+    fontSize: "15px",
+    paddingLeft: "2rem",
+    color: "#333",
+    lineHeight: "25px",
+  };
+  const imgStyle = {
+    margin: "10px auto",
+    display: "block",
+    height: "auto",
+    width: "150px",
+  };
+  const spanStyle = {
+    color: "#007ad6",
+  };
+  const divStyle = {
+    width: "300px",
+    display: "flex",
+    justifyContent: "center",
+    textAlign: "left",
+    flexDirection: "column",
+    height: "fit-content",
+    padding: "14px",
+    borderRadius: "6px",
+    border: "1px solid #ccc",
+    boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+  };
+ 
   return (
-    
-        <DIV >
-          <img src={props.img} alt={props.productName} />
-     <h2>{props.id} : {props.productName}  </h2>
-     <p>Credit Limit : <span>₹{props.creditLimit} </span> </p>
-     <p>Last Payment Amount : <span> ₹{props.lastPaymentAmount} </span></p>
-     <p>Last Payment Date : <span>{props.lastPaymentDate} </span> </p>
-     <p>Payment Due Amount :<span> ₹{props.paymentDueAmount} </span></p>
-     <p>Account Status : <span>{props.accountStatus} </span> </p>
+    <div style={divStyle}>
+      <img src={props.img} style={imgStyle} alt={props.productName} />
+      <h2 style={{ ...pStyle, fontWeight: "500" }}>
+        {props.id} : {props.productName}{" "}
+      </h2>
+      <p style={pStyle}>
+        Credit Limit : <span style={spanStyle}>₹{props.creditLimit} </span>{" "}
+      </p>
+      <p style={pStyle}>
+        Last Payment Amount :{" "}
+        <span style={spanStyle}> ₹{props.lastPaymentAmount} </span>
+      </p>
+      <p style={pStyle}>
+        Last Payment Date :{" "}
+        <span style={spanStyle}>{props.lastPaymentDate} </span>{" "}
+      </p>
+      <p style={pStyle}>
+        Payment Due Amount :
+        <span style={spanStyle}> ₹{props.paymentDueAmount} </span>
+      </p>
+      <p style={pStyle}>
+        Account Status : <span style={spanStyle}>{props.accountStatus} </span>{" "}
+      </p>
 
-     {/* CREDIT CARD SPECIFIC */}
-     {props.cashAdvanceLimit && <p>Cash Advance Limit : <span> ₹{props.cashAdvanceLimit} </span></p>}
-     {isAdmin && <button style={{background: '#23AAF2', borderRadius: '6px' , width: 'fit-content', margin: '8px auto', padding: '5px 30px', color:'#fff' }} onClick={editHandler} >Edit</button> }
+      {/* CREDIT CARD SPECIFIC */}
+      {props.cashAdvanceLimit && (
+        <p style={pStyle}>
+          Cash Advance Limit :{" "}
+          <span style={spanStyle}> ₹{props.cashAdvanceLimit} </span>
+        </p>
+      )}
+      {isAdmin && (
+        <button
+          style={{
+            background: "#23AAF2",
+            borderRadius: "6px",
+            width: "fit-content",
+            margin: "8px auto",
+            padding: "5px 30px",
+            color: "#fff",
+          }}
+          // onClick={}
+        >
+          Edit
+        </button>
+      )}
+    </div>
+  );
+};
 
-              </DIV> 
-    
-  )
-}
-
-export default Card
-
-const DIV = styled.div`
-width: 300px;
-display: flex;
-/* align-items: center; */
-justify-content: center;
-text-align: left;
-flex-direction: column;
-height: fit-content;
-padding: 14px;
-border-radius: 6px;
-
-border: 1px solid #ccc;
-box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-
-p,h2{
-  font-size: 15px;
-  padding-left: 2rem;
-  color: #333;
-  line-height: 25px;
-}
-h2{
-  font-weight: 500;
-}
-span{
-  color: #007ad6;
-}
-
-img{
-  margin: 10px auto;
-  display: block;
-  height: auto;
-  width: 150px;
-}
-  
-`;
+export default Card;
 
 
 
