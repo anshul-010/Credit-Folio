c0d16350a7876df3b1b90e4f11254b5cb41d2763
Anshul Kushwah <kushwahasg4450@gmail.com>
2023-07-29 15:19:51 +0530

    fakeCode

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import "../CSS/navbar.css"
 import { Link } from 'react-router-dom'
+import { AiOutlineMenu } from 'react-icons/ai'
 
 export const Topbar = () => {
   return (
     <div>
+      <AiOutlineMenu className='hamburgger' style={{display:'none'}}/>
         <div className='navbar' style={{border: 'none'}}>
             <div className='logo'><Link to='/'>Credit Folio</Link></div>
             <div className="pages">
               <Link to='/creditreport'><p>Credit Report</p></Link>
               <Link to='/education'><p>Credit Education</p></Link>
               <Link to='/helpcenter'><p>Help Center</p></Link>
-              <Link to='/about'><p>About</p></Link>
-              <Link to='/blog'><p>Blog</p></Link>
+              <Link to='/about'><p className='about-p' >About</p></Link>
+              <Link to='/blog'><p className='blog-p' >Blog</p></Link>
             </div>
             <div className='btn'>
-            <Link to='/login'> <button className='login-btn'>Log in</button></Link>
+            <Link to='/login'> <button className='login-btn'>Log In</button></Link>
             </div>
           
         </div>
